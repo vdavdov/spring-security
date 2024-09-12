730f0fd9068dcdff89ebdb6f176390cc906adda0
vdavdov <sevadavydov.06z@mail.ru>
2024-09-13 02:13:37 +0400

    Init commit

---
+++ src/test/java/by/vdavdov/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vdavdov.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
